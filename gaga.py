c7919ad4c72a53eb19c04931c63761457c017ed6
jo285317 <37353455+jo285317@users.noreply.github.com>
2019-09-09 02:59:09 -0400

    refactor

--- gaga.py
+++ gaga.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
-import spotipy
-from itertools import permutations
-import networkx as nx
-from textdistance import jaccard
-from spotipy.oauth2 import SpotifyClientCredentials
-import pickle
+
+import os
+try:
+	os.chdir(os.path.join(os.getcwd(), 'gaga'))
+	print(os.getcwd())
+except:
+	pass
+
+
+# In[ ]:
+
+
 
 class Song:
     def __init__(self, uri, name, artists):
@@ -18,97 +24,120 @@ class Album:
         self.name = name
         self.artist = artists
 
-client_id = ""
-client_secret = ""
-client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager) 
-name = "Lady Gaga"
-result = sp.search(name) 
-gag_uri = result['tracks']['items'][0]['artists'][0]['uri']
+#import spotipy
+from mining import albumSongs, artistAlbums, albumsSongs, useSearch, getSpotify
+import networkx as nx
+import pickle
+from itertools import permutations
+
 G = nx.Graph()
-known_albums = {}
 known_songs = {}
-
-
-def artistAlbums(sp, artist_uri, limit=50): # todo: more than 50!!
-    """ Returns a list of names,uris """
-    sp_albums = sp.artist_albums(artist_uri, album_type='album', limit=50)
-    albums = []
-    for album in sp_albums['items']:
-        if album['name'] not in album_names:
-            albums.append(Album(album['uri'], album['name'], album['artists']))
-    return albums
-
-def albumSongs(sp,album_uri, need_feats=True): 
-    """ Returns a list of names,uris """
-    songs = []
-    tracks = sp.album_tracks(album_uri) 
-    for track in tracks['items']: 
-        if len(track['artists']) > 1 and need_feats:
-            temp = []
-            for artist in track['artists']:
-                temp.append(artist['name']) 
-                songs.append(Song(track['uri'], track['name'], tuple(temp)))
-        elif not need_feats:
-            temp = []
-            for artist in track['artists']:
-                temp.append(artist['name']) 
-            songs.append(Song(track['uri'], track['name'], tuple(temp)))
-    return songs
-def albumsSongs(sp, albums, need_feats=True):
-    
-    """ Returns a list of names,uris for each album in the list"""
-    songs = []
-    for album in albums:
-        songs.extend(albumSongs(sp, album.uri))
-    return songs
-    
-def useSearch(sp, name, qtype='track', offset_=0, limit_=50, total=100, need_feats=True):
-    songs = []
-    while offset_ < total:
-        tracks = sp.search(q='artist:' + name, type=qtype, offset=offset_, limit=limit_)
-        offset_ += limit_
-        for track in tracks['tracks']['items']:
-            if len(track['artists']) > 1 and need_feats:
-                temp = ""
-                for artist in track['artists']:
-                    temp +=(artist['name']) + " " 
-                temp.strip()
-                songs.append(Song(track['uri'], track['name'], temp))
-            elif not need_feats:
-                temp = ""
-                for artist in track['artists']:
-                    temp +=(artist['name']) + " " 
-                temp.strip()
-                songs.append(Song(track['uri'], track['name'], temp))
-    return songs
-
-
-gaga_albums = artistAlbums(sp, gag_uri)
-gaga_f_songs = albumsSongs(sp, gaga_albums)
-
 known_artists = set()
+def updateSongsSearch(known_songs, known_artists,query):
+    newSongs = useSearch(sp, query)
+    addToSongs(newSongs, known_songs, known_artists)
 
-for song in gaga_f_songs:
-    known_songs[song.uri] = vars(song)
-    for artist in song.artists:
-        known_artists.add(artist)
-for artist in known_artists.copy():
-    result = sp.search(artist) 
-    artist_uri = result['tracks']['items'][0]['artists'][0]['uri']
-    artist_albums = artistAlbums(sp, artist_uri)
-    artist_songs = albumsSongs(sp, artist_albums)
-    for song in artist_songs:
+
+def addToSongs(songs, known_songs, known_artists):
+    for song in songs:
         known_songs[song.uri] = vars(song)
-        for artist_ in song.artists:
-            known_artists.add(artist_)
+        for artist in song.artists:
+            known_artists.add(artist)
 
-G.clear()
-for key, song in known_songs.items():
-    for artist in song['artists']:
-        G.add_node(artist)
-    perm = permutations(song['artists'])
-    for x in list(perm):
-        G.add_edge(x[0],x[1], val=song['name'])
-list(G.nodes)
-print(list(G.edges))
+def updateFromArtists(sp, user_given=None):
+    i = 0
+    use = known_artists
+    if user_given:
+        use = user_given
+    for artist in use:
+        if not G.has_node(artist):
+            G.add_node(artist)
+        if i > 30:
+            break
+        if  G.degree(artist) > 2:
+            print("skipp", end ="")
+            print(artist, end=" has degree")
+            print(G.degree(artist), end="\n")
+            i -= 1
+            pass
+        else:
+            print("ayy ", end="")
+            print(artist)
+            result = sp.search(artist) #search query
+            try:
+                artist_uri = result['tracks']['items'][0]['artists'][0]['uri']
+            except IndexError:
+                print("oops")
+                pass
+
+            artist_albums = artistAlbums(sp, artist_uri)
+            artist_songs = albumsSongs(sp, artist_albums)
+            i += 1
+            for song in artist_songs:
+                known_songs[song.uri] = vars(song)
+                for artist_ in song.artists:
+                    known_artists.add(artist_)
+
+def deserializeAndPopulate():
+    known_songs = {}
+    known_artists = set()
+    with open('kkk.pkl', 'rb') as handle:
+        known_songs = pickle.load(handle)
+    for key, song in known_songs.items():
+        for artist in song['artists']:
+            G.add_node(artist)
+            known_artists.add(artist)
+        perm = permutations(song['artists'])
+        for x in list(perm):
+            G.add_edge(x[0],x[1], edge_song=song['name'])
+    return known_songs, known_artists
+
+def save_obj(obj, name ):
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+save_obj(known_songs, "aftermulti-")
+def updateGraphFromSongs():
+    for key, song in known_songs.items():
+        for artist in song['artists']:
+            G.add_node(artist)
+            known_artists.add(artist)
+        perm = permutations(song['artists'])
+        for x in list(perm):
+            G.add_edge(x[0],x[1], edge_song=song['name'])
+
+def get_shortest_path(G, target):
+    try:
+        shortest_path = nx.shortest_path(G, source="Lady Gaga", target=target)
+    except nx.exception.NetworkXNoPath: 
+        return "∞", "INF"
+    outstr=""
+    for i in range(1,len(shortest_path)):
+        outstr += shortest_path[i-1] + " ---> "
+        data = G.edges[shortest_path[i-1], shortest_path[i]]['edge_song']
+        data_trimmed = data[:19] + (data[19:] and '...')
+        outstr += data_trimmed + " ---> "
+    outstr += shortest_path[-1]
+    return outstr, len(shortest_path) - 1
+    
+
+
+def expandLowDegree():
+    for artist in known_artists.copy():
+        if not G.has_node(artist):
+            G.add_node(artist)
+        if  G.degree(artist) < 2:
+            updateSongsSearch(known_songs, known_artists, artist)
+
+if __name__ == "__main__":
+    client_id = ""
+    client_secret = "" 
+    known_songs, known_artists = deserializeAndPopulate()
+    updateGraphFromSongs()
+    print(nx.info(G))
+    print(len(known_artists))
+    print(len(known_songs))
+
+    sp = getSpotify(client_id, client_secret)
+    # nx.write_graphml(G,"test.graphml")
+
+ 
